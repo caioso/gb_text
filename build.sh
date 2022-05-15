2fb19eecad325033670af877b6acabcbbb90c3c3
Caio Oliveira <caio.s.o@me.com>
2022-05-15 20:18:35 +0200

    [FEATURE] Implement register alias/unalias and add preprocessing to build flow

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-rgbasm -o main.o main.z80 -Weverything -Werror
-rgblink -o main.gbc -m main.map -n main.sym main.o
-rgbfix -v -p 0x00 main.gbc
+./scripts/rgbpre.py main.z80 build/main.pre.z80 &&
+rgbasm -o build/main.o build/main.pre.z80 -Weverything -Werror &&
+rgblink -o build/main.gbc -m build/main.map -n build/main.sym build/main.o &&
+rgbfix -v -p 0x00 build/main.gbc
--- scripts/rgbpre.py
+++ scripts/rgbpre.py
@@ -10,8 +10,8 @@ from typing import List
 
 sys.tracebacklimit = 0
 
-DEF_STRUCTURE_REGEX = r"^(\s)*def(\s)*(\w)+,(\s)*(\w)+(\s)*$"
-UNDEF_STRUCTURE_REGEX = r"^(\s)*undef(\s)*(\w)+(\s)*$"
+DEF_STRUCTURE_REGEX = r"^(\s)*!alias(\s)*(\w)+,(\s)*(\w)+(\s)*$"
+UNDEF_STRUCTURE_REGEX = r"^(\s)*!unalias(\s)*(\w)+(\s)*$"
 
 class Utils:
   @staticmethod
@@ -19,15 +19,23 @@ class Utils:
     index = line.find(';')
     return line if index == -1 else line[:index]
 
+  @staticmethod
+  def get_flat_text(text: List[str]) -> str:
+    flat_text = ""
+    for line in text:
+      flat_text += line
+
+    return flat_text
+
 class LabelOperation(Enum):
   DEF_LABEL = "DEF_LABEL"
   UNDEF_LABEL = "UNDEF_LABEL"
 
 class RegisterLabelOperation:
-  def __init__(self, position: int, label: str, register: str,
+  def __init__(self, position: int, alias: str, register: str,
                  op: LabelOperation):
     self._position = position
-    self._label = label
+    self._alias = alias
     self._register = register
     self._op = op
 
@@ -36,8 +44,8 @@ class RegisterLabelOperation:
     return self._position
 
   @property
-  def label(self) -> str:
-    return self._label
+  def alias(self) -> str:
+    return self._alias
 
   @property
   def register(self) -> str:
@@ -53,77 +61,86 @@ class RegisterLabelPass:
     self._raw_source = source
     self._input_file = input_file
 
-  def process(self) -> None:
-    def_labels = self._locate_defs()
-    undef_labels = self._locate_undefs()
+  @property
+  def processed_source(self) -> List[str]:
+    return self._processed_source
 
-    self._process_source(def_labels, undef_labels)
+  def process(self) -> None:
+    def_alias = self._locate_defs()
+    undef_alias = self._locate_undefs()
+
+    self._process_source(def_alias, undef_alias)
 
   def _locate_defs(self) -> List[RegisterLabelOperation]:
-    def_labels = []
+    def_alias = []
     for idx, line in enumerate(self._raw_source):
       clear_line = Utils.extract_line_no_comments(line)
 
       if re.match(DEF_STRUCTURE_REGEX, clear_line):
         self._raw_source[idx] = f"; {line}"
 
-        proper_command = re.split('def', clear_line)[1]
-        target_label = re.split(',', proper_command)[1].strip()
+        proper_command = re.split('!alias', clear_line)[1]
+        target_alias = re.split(',', proper_command)[1].strip()
         target_register = re.split(',', proper_command)[0].strip()
 
-        def_labels.append(RegisterLabelOperation(idx, target_label,
+        def_alias.append(RegisterLabelOperation(idx, target_alias,
                                                  target_register,
                                                  LabelOperation.DEF_LABEL))
-
-    return def_labels
+    return def_alias
 
   def _locate_undefs(self) -> List[RegisterLabelOperation]:
-    undef_labels = []
+    undef_alias = []
     for idx, line in enumerate(self._raw_source):
       clear_line = Utils.extract_line_no_comments(line)
 
       if re.match(UNDEF_STRUCTURE_REGEX, clear_line):
         self._raw_source[idx] = f"; {line}"
-        register_label = re.split('undef', clear_line)[1].strip()
-        undef_labels.append(RegisterLabelOperation(idx, register_label,
+        register_alias = re.split('!unalias', clear_line)[1].strip()
+        undef_alias.append(RegisterLabelOperation(idx, register_alias,
                                                  "", LabelOperation.DEF_LABEL))
-
-    return undef_labels
+    return undef_alias
 
   def _process_source(self, defs: List[RegisterLabelOperation],
                       undefs: List[RegisterLabelOperation]) -> None:
+    self._processed_source = []
     for idx, line in enumerate(self._raw_source):
       active_defs = self._locate_active_defs(defs, undefs, idx)
+      clear_line = Utils.extract_line_no_comments(line)
+      split_line = re.split('\s+', clear_line)
 
-      if len(active_defs) != 0:
-        label_list = ""
-        for label in active_defs:
-          label_list += f"{label.label}, "
+      line_text = line
+      for line_token in split_line:
+        for def_target in active_defs:
+          if def_target.alias in line_token:
+            line_text = line.replace(def_target.alias, def_target.register)
 
-        print(label_list)
-
+      self._processed_source.append(line_text)
 
   def _locate_active_defs(self, defs: List[RegisterLabelOperation],
                           undefs: List[RegisterLabelOperation],
                           index: int):
-    active_labels = []
+    active_alias = []
 
     for idx in range(0, index):
-      for label in defs:
-        if label.position == idx:
-          if len(active_labels) != 0 and \
-             len([x for x in active_labels if x.label == label.label]) != 0:
+      for alias in defs:
+        if alias.position == idx:
+          if len(active_alias) != 0 and \
+             len([x for x in active_alias if x.alias == alias.alias]) != 0:
             raise RuntimeError(f"{os.path.basename(self._input_file)} line " +
-                               f"{idx + 1}: label '{label.label}' has already been defined")
-          active_labels.append(label)
-      for label in undefs:
-        if label.position == idx:
-          if len(active_labels) == 0 or \
-             len([x for x in active_labels if x.label == label.label]) == 0:
-            raise RuntimeError(f"{idx + 1}: label '{label.label}' has not been defined")
-          active_labels = [x for x in active_labels if x.label != label.label]
+                               f"{idx + 1}: alias '{alias.alias}' has already been defined")
+          if len(active_alias) != 0 and \
+             len([x for x in active_alias if x.register == alias.register]) != 0:
+            raise RuntimeError(f"{os.path.basename(self._input_file)} line " +
+                               f"{idx + 1}: register '{alias.register}' has already been aliased")
+          active_alias.append(alias)
+      for alias in undefs:
+        if alias.position == idx:
+          if len(active_alias) == 0 or \
+             len([x for x in active_alias if x.alias == alias.alias]) == 0:
+            raise RuntimeError(f"{idx + 1}: alias '{alias.alias}' has not been defined")
+          active_alias = [x for x in active_alias if x.alias != alias.alias]
 
-    return active_labels
+    return active_alias
 
 def main():
   parser = argparse.ArgumentParser(description='rgb pre-processor')
@@ -144,8 +161,14 @@ def main():
 def process_file(input_file: str, output_file: str) -> None:
   file_source = load_file_source(input_file)
 
-  reg_label_pass = RegisterLabelPass(input_file, file_source)
-  reg_label_pass.process()
+  reg_alias_pass = RegisterLabelPass(input_file, file_source)
+  reg_alias_pass.process()
+
+
+  final_source = reg_alias_pass.processed_source
+  #save file
+  with open(output_file, 'w') as f:
+    f.write(Utils.get_flat_text(final_source))
 
 def load_file_source(file_path: str) -> List[str]:
   contents = []
